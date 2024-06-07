33db6763243f7518dfc19fd08c06a85d8920e795
Gabriel Gamboa <gabriel_gamboa10@hotmail.com>
2024-06-07 08:23:38 -0300

    feat: add user

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,8 +1,29 @@
 import fastify from "fastify";
-import { PrismaClient } from '@prisma/client'
+import z from 'zod';
+import { prisma } from "./infra/prisma";
 
 const app = fastify();
 
-const prisma = new PrismaClient();
+
+
+app.post('/users', async (request, reply) => {
+    const createUserSchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6)
+    })
+
+    const { email, name, password } = createUserSchema.parse(request.body);
+
+    await prisma.user.create({
+        data: { 
+            email,
+            name,
+            password_hash: password 
+        }
+    })
+
+    return reply.status(201).send();
+})
 
 export { app }
