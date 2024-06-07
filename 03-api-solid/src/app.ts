13f9f25dc3792e23d5ae2eb125740ac0ae4802ba
Gabriel Gamboa <gabriel_gamboa10@hotmail.com>
2024-06-07 08:40:38 -0300

    refactor: create http folder

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,29 +1,7 @@
 import fastify from "fastify";
-import z from 'zod';
-import { prisma } from "./infra/prisma";
+import { appRoutes } from "./infra/http/routes";
 
 const app = fastify();
-
-
-
-app.post('/users', async (request, reply) => {
-    const createUserSchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6)
-    })
-
-    const { email, name, password } = createUserSchema.parse(request.body);
-
-    await prisma.user.create({
-        data: { 
-            email,
-            name,
-            password_hash: password 
-        }
-    })
-
-    return reply.status(201).send();
-})
+app.register(appRoutes)
 
 export { app }
