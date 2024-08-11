529e7acd040e94c37992662e7fadc2db0c490a8c
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2024-08-11 12:00:25 -0300

    feat: history controller function

---
+++ 03-api-solid/src/infra/http/controllers/check-ins/history.ts
@@ -0,0 +1,19 @@
+import { makeGetUserCheckInHistoryUseCase } from "@/usecases/factories/make-get-user-check-in-history-usecase";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function history(request: FastifyRequest, reply: FastifyReply) {
+  const getUserCheckInHistorySchema = z.object({
+    page: z.coerce.number().min(1).default(1),
+  });
+
+  const { page } = getUserCheckInHistorySchema.parse(request.body);
+
+  const getCheckinHistoryUseCase = makeGetUserCheckInHistoryUseCase();
+  const { checkIns } = await getCheckinHistoryUseCase.execute({
+    userId: request.user.sub,
+    page,
+  });
+
+  return reply.status(200).send({ checkIns });
+}
