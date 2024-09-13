8a7b4ad794a5838a1cbf8702d210197d482790ad
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2024-09-13 12:27:03 -0300

    feat: new validation to validate jwt verify middleware

--- 03-api-solid/src/infra/middlewares/verify-jwt.ts
+++ 03-api-solid/src/infra/middlewares/verify-jwt.ts
@@ -2,6 +2,12 @@ import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function verifyJWT(request: FastifyRequest, reply: FastifyReply) {
   try {
+    const authorization = String(request.headers.authorization);
+
+    if (authorization.trim() === "" || !authorization.includes("Bearer")) {
+      return reply.status(401).send({ message: "Missing token" });
+    }
+
     await request.jwtVerify();
   } catch {
     return reply.status(401).send({ message: "Unauthorized" });
