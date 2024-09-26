194cc8d9f9fda232ec21f42f14eaf81ac156aaa3
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2024-09-26 12:37:12 -0300

    feat: add verify user role middleware in validate checkin

--- 03-api-solid/src/infra/http/routes/check-ins.routes.ts
+++ 03-api-solid/src/infra/http/routes/check-ins.routes.ts
@@ -4,12 +4,17 @@ import { validateCheckIn } from "../controllers/check-ins/validate";
 import { getMetrics } from "../controllers/check-ins/metrics";
 import { history } from "../controllers/check-ins/history";
 import { createCheckIn } from "../controllers/check-ins/create";
+import { verifyUserRole } from "@/infra/middlewares/verify-role";
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
 
   app.post("/:gymId", createCheckIn);
-  app.patch("/:checkInId/validate", validateCheckIn);
+  app.patch(
+    "/:checkInId/validate",
+    { onRequest: [verifyUserRole("ADMIN")] },
+    validateCheckIn
+  );
   app.get("/metrics", getMetrics);
   app.get("/history", history);
 }
