b920ce2b5b239f75de720aaab17578117ab889bf
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2024-09-26 12:37:01 -0300

    feat: create basic RBAC in application

--- 03-api-solid/src/infra/http/routes/gyms.routes.ts
+++ 03-api-solid/src/infra/http/routes/gyms.routes.ts
@@ -3,11 +3,12 @@ import { FastifyInstance } from "fastify";
 import { create } from "@/infra/http/controllers/gyms/create";
 import { search } from "../controllers/gyms/search";
 import { getNearby } from "../controllers/gyms/get-nearby";
+import { verifyUserRole } from "@/infra/middlewares/verify-role";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
 
-  app.post("/", create);
+  app.post("/", { onRequest: [verifyUserRole("ADMIN")] }, create);
   app.get("/search", search);
   app.get("/nearby", getNearby);
 }
