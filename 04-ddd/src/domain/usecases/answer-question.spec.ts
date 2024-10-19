ea0288df08bbcb9fe7065744dd1da605ec155480
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2024-10-19 10:56:19 -0300

    feat: create external dependency for answers repository

--- 04-ddd/src/domain/usecases/answer-question.spec.ts
+++ 04-ddd/src/domain/usecases/answer-question.spec.ts
@@ -1,9 +1,17 @@
 import { expect, test } from 'vitest';
 import { AnswerQuestionUseCase } from './answer-question';
+import { AnswersRepository } from '../repositories/answers-repository';
+import { Answer } from '../entities/answer';
 
-test('create an answer', () => {
-  const createAnswerUseCase = new AnswerQuestionUseCase();
-  const response = createAnswerUseCase.execute({ content: 'Nova Resposta', questionId: '1', instructorId: '1' });
+const fakeAnswersRepository: AnswersRepository = {
+  create: function (answer: Answer): Promise<void> {
+    return Promise.resolve();
+  }
+}
+
+test('create an answer', async () => {
+  const createAnswerUseCase = new AnswerQuestionUseCase(fakeAnswersRepository);
+  const response = await createAnswerUseCase.execute({ content: 'Nova Resposta', questionId: '1', instructorId: '1' });
 
   expect(response.content).toEqual('Nova Resposta');
 })
