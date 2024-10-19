9d74ea65e586bbc9f1851d28cfbce9ec703b3d54
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2024-10-19 11:29:56 -0300

    refactor: comment to explain protected property from base Entity

--- 04-ddd/src/core/entities/entity.ts
+++ 04-ddd/src/core/entities/entity.ts
@@ -2,7 +2,7 @@ import { randomUUID } from "crypto";
 
 export class Entity<Props> {
   private _id: string;
-  protected props: Props;
+  protected props: Props; //protected allows to just access it from the class that extends Entity
 
   constructor(props: Props, id?: string) {
     this._id = id ?? randomUUID();
