fa2212e1483bf6570514baf7ce9f1bc8f1c74112
Gabrie Gambôa <gabriel_gamboa10@hotmail.com>
2024-11-09 12:17:46 -0300

    refactor: add config to avoid errors from prettier end of line

--- 04-ddd/test/factories/make-answer.ts
+++ 04-ddd/test/factories/make-answer.ts
@@ -1,16 +1,16 @@
-import { faker } from "@faker-js/faker";
-import { Id } from "@/core/entities/id";
-import { Answer, AnswerProps } from "@/domain/forum/enterprise/entities/answer";
+import { faker } from '@faker-js/faker'
+import { Id } from '@/core/entities/id'
+import { Answer, AnswerProps } from '@/domain/forum/enterprise/entities/answer'
 
 export const makeAnswer = (override: Partial<AnswerProps> = {}, id?: Id) => {
   return Answer.create(
     {
-      authorId: new Id("1"),
+      authorId: new Id('1'),
       content: faker.lorem.sentence(),
-      questionId: new Id("1"),
+      questionId: new Id('1'),
       createdAt: new Date(),
       ...override,
     },
     id,
-  );
-};
+  )
+}
--- 04-ddd/test/factories/make-question.ts
+++ 04-ddd/test/factories/make-question.ts
@@ -1,10 +1,10 @@
-import { faker } from "@faker-js/faker";
-import { Id } from "@/core/entities/id";
+import { faker } from '@faker-js/faker'
+import { Id } from '@/core/entities/id'
 import {
   Question,
   QuestionProps,
-} from "@/domain/forum/enterprise/entities/question";
-import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
+} from '@/domain/forum/enterprise/entities/question'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
 export const makeQuestion = (
   override: Partial<QuestionProps> = {},
@@ -12,13 +12,13 @@ export const makeQuestion = (
 ) => {
   return Question.create(
     {
-      authorId: new Id("1"),
+      authorId: new Id('1'),
       content: faker.lorem.sentence(),
       title: faker.lorem.text(),
-      slug: new Slug("nova-pergunta"),
+      slug: new Slug('nova-pergunta'),
       createdAt: new Date(),
       ...override,
     },
     id,
-  );
-};
+  )
+}
