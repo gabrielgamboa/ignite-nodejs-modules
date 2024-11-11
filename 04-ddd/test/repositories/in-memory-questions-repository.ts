795e1a956a1362c9291d2651a3b3d70d79b4d503
Gabrie Gambôa <gabriel_gamboa10@hotmail.com>
2024-11-11 00:03:54 -0300

    refactor: change name file with in-memory prefix

---
+++ 04-ddd/test/repositories/in-memory-questions-repository.ts
@@ -0,0 +1,45 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
+import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
+import { Question } from "@/domain/forum/enterprise/entities/question";
+
+export class InMemoryQuestionsRepository implements QuestionsRepository {
+  public items: Question[] = [];
+
+  async create(question: Question): Promise<void> {
+    this.items.push(question);
+  }
+
+  async findBySlug(slug: string): Promise<Question | null> {
+    return this.items.find((item) => item.slug.value === slug) ?? null;
+  }
+
+  async findById(id: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.id.toString() === id);
+
+    if (!question) return null;
+
+    return question;
+  }
+
+  async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
+    return this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20);
+  }
+
+  async delete(question: Question): Promise<void> {
+    const index = this.items.findIndex(
+      (item) => item.id.toString() === question.id.toString(),
+    );
+
+    this.items.splice(index, 1);
+  }
+
+  async save(question: Question): Promise<void> {
+    const index = this.items.findIndex(
+      (item) => item.id.toString() === question.id.toString(),
+    );
+
+    this.items[index] = question;
+  }
+}
