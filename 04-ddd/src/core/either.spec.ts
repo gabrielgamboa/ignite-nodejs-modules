617728858c05e6372bc95a5e79059b1dbe8108a3
Gabrie Gambôa <gabriel_gamboa10@hotmail.com>
2024-11-15 02:27:42 -0300

    feat: implement more either

---
+++ 04-ddd/src/core/either.spec.ts
@@ -0,0 +1,23 @@
+import { Either, left, right } from "./either"
+
+function doSomething(shouldSuccess: boolean): Either<string, string> {
+  if (shouldSuccess) {
+    return left('success')
+  } else {
+    return right('error');
+  }
+}
+
+test('success result', () => {
+  const result = doSomething(true);
+
+  expect(result.isLeft()).toBe(true);
+  expect(result.isRight()).toBe(false);
+})
+
+test('error result', () => {
+  const result = doSomething(false);
+
+  expect(result.isRight()).toBe(true);
+  expect(result.isLeft()).toBe(false);
+})
