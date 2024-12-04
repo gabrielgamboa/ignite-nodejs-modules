013945a706879d65174cce89f4406ee1d03236f6
Gabrie Gambôa <gabriel_gamboa10@hotmail.com>
2024-12-03 21:32:50 -0300

    chore: add bcrypt in project

--- 05-nest/src/controllers/create-account.controller.ts
+++ 05-nest/src/controllers/create-account.controller.ts
@@ -1,5 +1,6 @@
 import { Body, ConflictException, Controller, HttpCode, Post } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { hash } from 'bcryptjs';
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -20,6 +21,8 @@ export class CreateAccountController {
       throw new ConflictException('User with same email address already exists');
     }
 
-    await this.prismaService.user.create({ data: { name, email, password } });
+    const hashedPassword = await hash(password, 8);
+
+    await this.prismaService.user.create({ data: { name, email, password: hashedPassword } });
   }
 }
