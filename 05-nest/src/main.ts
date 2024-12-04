8817561ae9ecaa209e680a21e81958a3744ac005
Gabrie Gambôa <gabriel_gamboa10@hotmail.com>
2024-12-04 17:08:33 -0300

    envs: setup config module

--- 05-nest/src/main.ts
+++ 05-nest/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Env } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3333);
+
+  //tipa o config service com Env, e força dizendo que as variáveis já estão validadas
+  const configService: ConfigService<Env, true> = app.get(ConfigService);
+
+  //inferir o tipo de port para ser number, se não vai acusar de ser tipo any
+  const port = configService.get('PORT', { infer: true });
+
+  await app.listen(port);
 }
 bootstrap();
