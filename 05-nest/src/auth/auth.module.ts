325cd52a4ed07c511cb8afd4e495864fb5152767
Gabrie Gambôa <gabriel_gamboa10@hotmail.com>
2024-12-07 10:26:43 -0300

    feat: use rs256 to generate jwt

---
+++ 05-nest/src/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { Env } from "src/env";
+
+@Module({
+  imports: [JwtModule.registerAsync({
+    inject: [ConfigService],
+    global: true,
+    useFactory(config: ConfigService<Env, true>) {
+      const privateKey = config.get('JWT_PRIVATE_KEY');
+      const publicKey = config.get('JWT_PUBLIC_KEY');
+
+      return {
+        signOptions: { algorithm: 'RS256' },
+        publicKey: Buffer.from(publicKey, 'base64'),
+        privateKey: Buffer.from(privateKey, 'base64'),
+      }
+    }
+  })]
+})
+export class AuthModule {
+
+}
---
+++ 05-nest/src/controllers/authenticate.controller.ts
@@ -0,0 +1,48 @@
+import { Body, Controller, HttpCode, Post, UnauthorizedException, UsePipes } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { compare } from 'bcryptjs';
+import { z } from 'zod';
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { JwtService } from "@nestjs/jwt";
+
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
+
+@Controller('/sessions')
+export class AuthenticateController {
+
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService,
+  ) { }
+
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() data: AuthenticateBodySchema) {
+    const { email, password } = data;
+
+    const user = await this.prismaService.user.findUnique({ where: { email } });
+
+    if (!user) {
+      throw new UnauthorizedException('Email or password incorrect');
+    }
+
+    const isSamePassword = await compare(password, user.password);
+
+    if (!isSamePassword) {
+      throw new UnauthorizedException('Email or password incorrect');
+    }
+
+    const accessToken = this.jwtService.sign({ sub: user.id });
+
+    return {
+      access_token: accessToken
+    }
+  }
+}
